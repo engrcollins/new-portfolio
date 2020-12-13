8d19db8d82f7aac6ad15f9981feb3ff23366f4e1
engrcollins <akinbamicollins@gmail.com>
2020-12-13 19:50:57 +0000

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,7 +106,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: settings.meta.iconimage,
+        icon: "static" + settings.meta.iconimage,
       },
     },
     'gatsby-plugin-offline',
